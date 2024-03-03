65f0a444067fbd014a1f03770e57f607259626af
hectdiazz <162059161+hectdiazz@users.noreply.github.com>
2024-03-03 16:29:35 -0700

    Update ip_info.py
    Added comments to show our progress and to assist in knowing what parts of the code does what

--- ip_info.py
+++ ip_info.py
@@ -4,7 +4,7 @@
 
 import requests
 
-def get_ip_info(api_key):
+def get_ip_info(api_key):                 #This is defining the API, ensuring this API works with the provided key
     url = f'http://api.ipstack.com/check'
     params = {'access_key': api_key}
 
@@ -19,15 +19,15 @@ def get_ip_info(api_key):
         print(f"Error: {e}")
         return None
 
-def main():
+def main():                                    #this is defining the main code using we define the API key here
     api_key = '8d12eee976f4d115e89b3561b3d5fff4'
     ip_info = get_ip_info(api_key)
     if ip_info:
-        print("IP Address:", ip_info.get('ip'))
-        print("Country:", ip_info.get('country_name'))
-        print("Type:", ip_info.get('type'))
+        print("IP Address:", ip_info.get('ip')) #This is requesting the IP address of the current user
+        print("Country:", ip_info.get('country_name')) #The feature pulls the country of the user
+        print("Type:", ip_info.get('type')) #This shows the type of connection, whether an IPV4 or IPV6
     else:
-        print("Failed to retrieve IP information.")
+        print("Failed to retrieve IP information.") #This gives us a failure if the code is run without any IP addressing information
 
 if __name__ == "__main__":
     main()
